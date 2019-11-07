d6a10f539f78ce02b0c8bee51d166a34b2e05fb4
Maël Kerbiriou <piezo.w.dimd@gmail.com>
2019-11-07 19:33:58 +0000

    fixup! fixes for c++11 compatibility

--- include/gatbl/utils/condition_check.hpp
+++ include/gatbl/utils/condition_check.hpp
@@ -22,7 +22,9 @@ struct condition_check
     void set(bool checked) const { _checked = checked; }
     void unchecked() const { _checked = false; }
     void checked() const { _checked = true; }
-    void check(const char file[] = __FILE__, const char fun[] = __PRETTY_FUNCTION__, unsigned line = __LINE__) const
+    void check(const char file[] = __builtin_FILE(),
+               const char fun[]  = __builtin_FUNCTION(),
+               unsigned   line   = __builtin_LINE()) const
     {
         if (unlikely(!_checked)) { abort_message("%s:%s:%u: condition check failed", file, fun, line); }
     }
